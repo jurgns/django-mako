f4e14334b345a0dc68b34f5c970a2f431e2a7b26
mikeal.rogers <mikeal.rogers@1c3627a2-86d3-11dd-99d6-6900107db72b>
2008-09-20 05:28:37 +0000

    Initial checkin.

---
+++ djangomako/shortcuts.py
@@ -0,0 +1,30 @@
+#   Copyright (c) 2008 Mikeal Rogers
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from django.http import HttpResponse
+
+import middleware
+
+def render_to_string(template_name, **dictionary):
+    template = middleware.lookup.get_template(template_name)
+    result = template.render(**dictionary)
+    return result
+
+def render_to_response(*args, **kwargs):
+    """
+    Returns a HttpResponse whose content is filled with the result of calling
+    lookup.get_template(args[0]).render with the passed arguments.
+    """
+    httpresponse_kwargs = {'mimetype': kwargs.pop('mimetype', None)}
+    return HttpResponse(render_to_string(*args, **kwargs), **httpresponse_kwargs)
---
+++ setup.py
@@ -0,0 +1,44 @@
+#   Copyright (c) 2008 Mikeal Rogers
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from setuptools import setup, find_packages
+import os, sys
+
+PACKAGE_NAME = "django-mako"
+PACKAGE_VERSION = "0.1.0pre"
+
+SUMMARY = 'Django Mako'
+
+DESCRIPTION = """This module provides a drop in replacement of django templates for mako templates."""
+
+setup(name=PACKAGE_NAME,
+      version=PACKAGE_VERSION,
+      description=SUMMARY,
+      long_description=DESCRIPTION,
+      author='Mikeal Rogers',
+      author_email='mikeal.rogers@gmail.com',
+      url='http://code.google.com/p/django-mako/',
+      license='Apache 2.0',
+      include_package_data = True,
+      packages = find_packages(),
+      platforms =['Any'],
+      install_requires = ['django', 'mako'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Operating System :: OS Independent',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                  ],
+     )
